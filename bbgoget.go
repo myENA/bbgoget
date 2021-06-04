0335a699a74d493a9e34d6ff32df940cd0577b76
Nathan Johnson <njohnson@ena.com>
2021-06-04 08:39:23 -0500

    adding depth flag

--- bbgoget.go
+++ bbgoget.go
@@ -17,17 +17,20 @@ func main() {
 		sshPort            int
 		listenAddr         string
 		serverNameOverride string
+		depth              int
 	)
 
 	fs.StringVar(&listenAddr, "listen-address", ":8800", "specify listen address")
-	fs.IntVar(&sshPort, "ssh-port", 7999, "specify bitbucket ssh port")
+	fs.IntVar(&sshPort, "ssh-port", 7999, "specify git server ssh port")
 	fs.StringVar(&serverNameOverride, "servername-override", "", "override the server name.  if not specified, uses the host value from the X-Forwarded-Host header")
+	fs.IntVar(&depth, "depth", 3, "specify depth of repositories")
 
 	_ = fs.Parse(os.Args[1:])
 
 	bbh := &BBHandler{
 		sshPort:            sshPort,
 		serverNameOverride: serverNameOverride,
+		depth:              depth,
 	}
 	server := &http.Server{
 		Addr:              listenAddr,
@@ -46,6 +49,7 @@ func main() {
 type BBHandler struct {
 	serverNameOverride string
 	sshPort            int
+	depth              int
 }
 
 func (bbh *BBHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
@@ -56,19 +60,17 @@ func (bbh *BBHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Since path should begin with '/', strings.split
-	// will return the first element as blank, second
-	// as project, third as our actual repository.
+
 	pathParts := strings.Split(req.URL.Path, "/")
 
-	if len(pathParts) < 3 {
+	if len(pathParts) < bbh.depth {
 		resp.WriteHeader(http.StatusBadRequest)
 		resp.Write([]byte("invalid path\n"))
 		return
 	}
 
 	// Strip everything after the repository portion of the URL
-	prefix := strings.Join(pathParts[:3], "/")
+	prefix := strings.Join(pathParts[:bbh.depth], "/")
 
 	// Try to find the host
 	host := bbh.serverNameOverride
@@ -85,7 +87,11 @@ func (bbh *BBHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 		resp.Write([]byte("blank host"))
 	}
 
-	repoURL := fmt.Sprintf("ssh://git@%s:%d%s.git", host, bbh.sshPort, prefix)
+	hostSuffix := ""
+	if bbh.sshPort != 22 {
+		hostSuffix = fmt.Sprintf(":%d", bbh.sshPort)
+	}
+	repoURL := fmt.Sprintf("ssh://git@%s%s%s.git", host, hostSuffix, prefix)
 	importPrefix := host + prefix
 
 	resp.WriteHeader(http.StatusOK)
